c0f57e4fd06d51d9d703e2b0a22b1f5f10f79c5f
SoBoLp <pbolotov@gmail.com>
2019-02-05 18:34:28 +0100

    2.py done

--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,22 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+
+def getAllVariants (l, in_str):
+    # print(l)
+    if len(in_str) == 2:
+        return l.append(in_str[0:])
+    for i in range(1,len(in_str)):
+        l.append(in_str[i:])
+        getAllVariants(l, in_str[i:])
+        l.append(in_str[:i])
+        getAllVariants(l, in_str[:i])
+
+
+
+l = []
+getAllVariants(l, "papa")
+unique_list = list(set(l))
+print(unique_list)
+print(len(unique_list))
+
